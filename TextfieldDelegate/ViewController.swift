035574242bde21f6148b37bde2073a48b90a91e3
Marco <Marco@Marcos-iMac.fritz.box>
2015-01-07 20:31:34 +0100

    Anfang

--- TextfieldDelegate/ViewController.swift
+++ TextfieldDelegate/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var data : String?
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBOutlet weak var textArea: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    textField.delegate = self
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+
+
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        saveLogic()
+        dismissKeyboard()
     }
 
-
+    // textField nochmal als Action mit ...has Changed conectedjjjj
+    @IBAction func textFieldDidChange(sender: AnyObject) {
+    
+        saveLogic()
+    
+    }
+    
+    func saveLogic() {
+        data = textField.text
+        textArea.text = data
+        println("Data: \(data)")
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        saveLogic()
+        dismissKeyboard()
+        return true
+    }
+    
+    func dismissKeyboard()  {
+        view.endEditing(false)
+    
+    }
 }
+
+
 
